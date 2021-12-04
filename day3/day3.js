b2d226b951eaead69c8350aeea0c3299a7bd0857
Zach Willard <zach.willard@gmail.com>
2021-12-04 09:25:08 -0600

    feat: add mostCommon func

--- day3/day3.js
+++ day3/day3.js
@@ -6,35 +6,28 @@ function parse(diagnosticData) {
 
 // Creates an array with 0's matching the length of the given data. eg [0,0,0,0,0]. so we can handle any kind of data set
 function generateCountingArray(data) {
-  return [...'0'.repeat(data.length)].map((value) => +value)
+  return [...'0'.repeat(data.length)].map((value) => Number.parseInt(value))
 }
 
 function calcGammaBits(data) {
   const countingArray = generateCountingArray(data[0])
   const combined = data.reduce((acc, currentReading) => {
     // for each position number, add up the bits
-    return acc.map((value, index) => value + +currentReading[index]) // + is needed before currentReading[index] to coerce it to a number
+    return acc.map(
+      (value, index) => value + Number.parseInt(currentReading[index])
+    )
   }, countingArray)
 
   const halfLength = data.length / 2
-  const gammaBits = combined.map((bitCount) => {
-    if (bitCount > halfLength) {
-      // then we know 1s are common
-      return 1
-    }
-    return 0
-  })
+  const gammaBits = combined.map((bitCount) => (bitCount > halfLength ? 1 : 0))
 
   return arrayToString(gammaBits)
 }
 
 function flipBits(binaryString) {
-  const flipped = [...binaryString].map((bit) => {
-    if (+bit === 0) {
-      return 1
-    }
-    return 0
-  })
+  const flipped = [...binaryString].map((bit) =>
+    Number.parseInt(bit) === 0 ? 1 : 0
+  )
 
   return arrayToString(flipped)
 }
@@ -45,20 +38,46 @@ function binaryStringToBase10(binaryString) {
   return Number.parseInt(binaryString, 2)
 }
 
-function solution(){
+function part1solution() {
   const parsedData = parse(diagnostics)
   const gammaBits = calcGammaBits(parsedData)
   const gammaRate = binaryStringToBase10(gammaBits)
-
 
   const epsilonBits = flipBits(gammaBits)
   const epsilonRate = binaryStringToBase10(epsilonBits)
 
   const answer = gammaRate * epsilonRate
-  console.log(gammaRate * epsilonRate)
   return answer
 }
 
-solution()
+// Part 2 functions
 
-export { parse, calcGammaBits, flipBits, binaryStringToBase10, solution }
+function mostCommon(arr, bitIndex) {
+  // return values: most common value, or -1 for a tie
+  const result = arr.reduce(
+    (counts, value) => {
+      const { zeros, ones } = counts
+      return Number.parseInt(value[bitIndex]) === 0
+        ? { zeros: zeros + 1, ones }
+        : { zeros, ones: ones + 1 }
+    },
+    { zeros: 0, ones: 0 }
+  )
+
+  const { zeros, ones } = result
+  if (zeros > ones) {
+    return 0
+  } else if (zeros < ones) {
+    return 1
+  }
+  return -1
+}
+
+export {
+  parse,
+  calcGammaBits,
+  flipBits,
+  binaryStringToBase10,
+  part1solution,
+  mostCommon,
+}
--- day3/day3.test.js
+++ day3/day3.test.js
@@ -1,6 +1,13 @@
-import { parse, calcGammaBits, flipBits, binaryStringToBase10 } from './day3.js'
+import {
+  parse,
+  calcGammaBits,
+  flipBits,
+  binaryStringToBase10,
+  part1solution,
+  mostCommon,
+} from './day3.js'
 
-describe('Day 3 - parse data', () => {
+describe('parse data', () => {
   test('example data should parse correctly', () => {
     const example = `00100
 11110
@@ -32,7 +39,7 @@ describe('Day 3 - parse data', () => {
   })
 })
 
-describe('Day 3 - calcGammaBits', () => {
+describe('calcGammaBits', () => {
   test('calculates example data correctly', () => {
     const example = `00100
 11110
@@ -51,18 +58,74 @@ describe('Day 3 - calcGammaBits', () => {
   })
 })
 
-describe('Day 3 - flipBits', () => {
+describe('flipBits', () => {
   test('flips 10010 to 01101', () => {
     expect(flipBits('10010')).toBe('01101')
   })
 })
 
-describe('Day 3 - binaryStringToBase10', () => {
+describe('binaryStringToBase10', () => {
   test('10110 returns 22', () => {
     expect(binaryStringToBase10('10110')).toEqual(22)
   })
 
   test('01001 returns 9', () => {
     expect(binaryStringToBase10('01001')).toEqual(9)
+  })
+})
+
+describe('Part 1 solution', () => {
+  test('part1solution() returns 2743844', () => {
+    expect(part1solution()).toEqual(2743844)
+  })
+})
+
+describe('Part 2 - mostCommon', () => {
+  test('given 1s most common, mostCommon should return 1', () => {
+    const exampleData = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+    expect(mostCommon(parse(exampleData), 0)).toEqual(1)
+  })
+
+  test('given a tie, mostCommon should return -1', () => {
+    const exampleData = `00100
+11110
+10110
+10111
+00101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+    expect(mostCommon(parse(exampleData), 0)).toEqual(-1)
+  })
+
+  test('given 0s most common, mostCommon should return 0', () => {
+    const exampleData = `00100
+11110
+10110
+00111
+00101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+    expect(mostCommon(parse(exampleData), 0)).toEqual(0)
   })
 })
