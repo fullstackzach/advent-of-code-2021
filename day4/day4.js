b28cc671827767007c0df2cec97c882697aa1318
Zach Willard <13011363+fullstackzach@users.noreply.github.com>
2021-12-13 23:31:32 -0600

    lint

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,10 @@
 import fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-
-function getRawData () {
+function getRawData() {
   try {
     const data = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8')
     return data
@@ -14,11 +13,10 @@ function getRawData () {
   }
 }
 
-
 class Spot {
   constructor(number) {
     this.marked = false
-    this.number = parseInt(number)
+    this.number = Number.parseInt(number)
   }
   mark() {
     this.marked = true
@@ -27,9 +25,9 @@ class Spot {
 
 class BingoBoard {
   constructor(rawBoard) {
-    this.rows = rawBoard.map(row => {
+    this.rows = rawBoard.map((row) => {
       const rowArr = row.trim().split(/\s+/)
-      return rowArr.map(number => new Spot(number))
+      return rowArr.map((number) => new Spot(number))
     })
   }
 
@@ -45,7 +43,7 @@ class BingoBoard {
   isBingo() {
     // any horizontal
     for (const row of this.rows) {
-      if (row.every(spot => spot.marked)) return true
+      if (row.every((spot) => spot.marked)) return true
     }
     // any vertical
     for (let i = 0; i < this.rows[0].length; i++) {
@@ -64,7 +62,7 @@ class BingoBoard {
   calcSumOfUnmarked() {
     let sumUnmarked = 0
     for (const row of this.rows) {
-      row.forEach(spot => {
+      row.forEach((spot) => {
         if (!spot.marked) {
           sumUnmarked += spot.number
         }
@@ -73,7 +71,6 @@ class BingoBoard {
     return sumUnmarked
   }
 }
-
 
 const data = getRawData()
 const lines = data.split('\n')
@@ -85,8 +82,8 @@ let rawBoard = []
 let boardCollection = []
 for (const line of lines) {
   if (index === 0) {
-    callouts = line.split(',').map(callout => parseInt(callout))
-  } else if (line === '')  {
+    callouts = line.split(',').map((callout) => Number.parseInt(callout))
+  } else if (line === '') {
     rawBoard = []
   } else {
     rawBoard.push(line)
@@ -100,23 +97,25 @@ for (const line of lines) {
   index++
 }
 
-function runTheGame () {
+function runTheGame() {
   let winningScore
   callouts.every((callout, index) => {
     boardCollection.every((board, boardIndex) => {
       board.checkAndMark(callout)
-      if (index >= 5) {
-        if (board.isBingo()){
-          const unmarkedSum = board.calcSumOfUnmarked()
-          winningScore = callout * unmarkedSum
-          console.log(`Bingo! Board ${boardIndex} won the game in ${index+1} moves on the called number ${callout}! The unmarked sum was ${unmarkedSum}, for a total score of ${winningScore}`)
-          return false; // acts like a break statement in an "every"
-        }
+      if (index >= 5 && board.isBingo()) {
+        const unmarkedSum = board.calcSumOfUnmarked()
+        winningScore = callout * unmarkedSum
+        console.log(
+          `Bingo! Board ${boardIndex} won the game in ${
+            index + 1
+          } moves on the called number ${callout}! The unmarked sum was ${unmarkedSum}, for a total score of ${winningScore}`
+        )
+        return false // acts like a break statement in an "every"
       }
-      return true;
+      return true
     })
-    if (winningScore) return false;
-    return true;
+    if (winningScore) return false
+    return true
   })
   return winningScore
 }
